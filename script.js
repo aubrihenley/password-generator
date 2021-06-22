013c80930a0df28b1254a924b69e494b3014b7c3
Aubri Henley <aubri.henley@gmail.com>
2021-06-22 17:20:41 -0400

    created functions to get random number, uppercase, and lowercase

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Generate password functions for selected password options ie.uppercase, lowercase, numbers, characters
+
+//Created functions using character codes 
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+ 97);
+}
+console.log(getRandomLower());
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+ 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random()*10)+ 48);
+}
+
+
+
+
+
+
