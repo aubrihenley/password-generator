8a9e62c93f18949c25f289e43eef72dd5e7e7367
Aubri Henley <aubri.henley@gmail.com>
2021-06-23 14:41:59 -0400

    changed character function variables to arrays

--- script.js
+++ script.js
@@ -13,44 +13,36 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword()
+
+// creating variables for character functions
+
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var symbol = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+
 // Generate password functions for selected password options ie.uppercase, lowercase, numbers, characters
-
-//Created functions using character codes 
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random()*26)+ 97);
-}
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random()*26)+ 65);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random()*10)+ 48);
-}
-
-function getRandomSymbol() {
-  return String.fromCharCode(Math.floor(Math.random()*15)+ 33);
-}
-
 
 // created prompts for selecting password options
 var pswdLength = Number(prompt("How many characters would you like your password to be?"));
   while (isNaN(length) || length < 8 || length > 128) length= Number(prompt("Length must be 8-128 characters. Please verify how long you would like your password to be."));
 
-var uppers = confirm("Click 'okay' if your password will include uppercase letters?");
-var lowers = confirm("Click 'okay' if your password will include lowercase letters?");
-var numbers = confirm("Click 'okay' if your password will include numbers?");
-var symbols = confirm("Click 'okay' if your password will include symbols?");
+var getRandomUpper = confirm("Click 'OK' if your password will include uppercase letters?");
+var getRandomLower = confirm("Click 'OK' if your password will include lowercase letters?");
+var getRandomNumber = confirm("Click 'OK' if your password will include numbers?");
+var getRandomSymbol = confirm("Click 'OK' if your password will include symbols?");
 
 //if no options are selected, alert and run through prompts again.
-while (!uppers && !lowers && !numbers && !symbols) {
+while (!getRandomUpper && !getRandomLower && !getRandomNumber && !getRandomSymbol) {
   alert("You must select at least one character type!")
-  uppers = confirm("Click 'okay' if your password will uppercase letters?");
-  lowers = confirm("Click 'okay' if your password will lowercase letters?");
-  numbers = confirm("Click 'okay' if your password will numbers?");
-  symbols = confirm("Click 'okay' if your password will symbols?");
-
-}
-
+  getRandomUpper = confirm("Click 'OK' if your password will include uppercase letters?");
+  getRandomLower  = confirm("Click 'OK' if your password will include lowercase letters?");
+  getRandomNumber = confirm("Click 'OK' if your password will include numbers?");
+  getRandomSymbol = confirm("Click 'OK' if your password willinclude symbols?");
+};
 
